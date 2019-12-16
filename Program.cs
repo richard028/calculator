d4e755e10466426d07029b95e86bbb0310ecd9eb
richard028 <58846414+richard028@users.noreply.github.com>
2019-12-16 14:39:18 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace calculator_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double first;
+            double second;
+            double third;
+            string symbol;
+            Console.WriteLine("Enter First Number:");
+            first = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Second Number:");
+            second = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Select an operator + for addition, - for subtraction, * for multiplication, / for division.");
+            symbol = Console.ReadLine();
+
+            if (symbol == "-")
+            {
+                third = first - second;
+                Console.WriteLine("{0} {1} {2} = {3}.", first, symbol, second, third);
+                Console.ReadLine();
+            }
+            else if (symbol == "+")
+            {
+                third = first + second;
+                Console.WriteLine("{0} {1} {2} = {3}.", first, symbol, second, third);
+                Console.ReadLine();
+            }
+            else if (symbol == "*")
+            {
+                third = first * second;
+                Console.WriteLine("{0} {1} {2} = {3}.", first, symbol, second, third);
+                Console.ReadLine();
+            }
+            else if (symbol == "/")
+            {
+                third = first / second;
+                Console.WriteLine("{0} {1} {2} = {3}", first, symbol, second, third);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Error");
+                Console.ReadLine();
+            }
+        }
+    }
+}
